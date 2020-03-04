b5872571d1d45efc050cfc72aa48fe6e215d7e0b
navintati <navin.tatikonda@genuus.in>
2020-03-04 18:24:18 +0530

    Crud Completed

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -2,9 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PostController extends Controller
 {
@@ -13,7 +18,8 @@ class PostController extends Controller
      */
     public function showAllPostAction(Request $request)
     {
-        return $this->render('pages/index.html.twig');
+        $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findAll();
+        return $this->render('pages/index.html.twig', ['posts' => $posts]);
     }
 
     /**
@@ -21,7 +27,34 @@ class PostController extends Controller
      */
     public function createPostAction(Request $request)
     {
-        return $this->render('pages/create.html.twig');
+        $post = new Post;
+        $form = $this->createFormBuilder($post)
+        ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control')))
+        ->add('category', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array('label' => 'Create Post', 'attr' => array('class' => 'btn btn-success', 'style' => 'margin-top:20px')))
+        ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $title = $form['title']->getData();
+            $description = $form['description']->getData();
+            $category = $form['category']->getData();
+
+            $post->setTitle($title);
+            $post->setDescription($description);
+            $post->setCategory($category);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            $this->addFlash('message', 'Post Created Successfully');
+            return $this->redirectToRoute('show_post_route');
+
+        }
+        return $this->render('pages/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
@@ -29,15 +62,49 @@ class PostController extends Controller
      */
     public function viewPostAction($id)
     {
-        return $this->render('pages/view.html.twig');
+        $post = $this->getDoctrine()->getRepository('AppBundle:Post')->find($id);
+        return $this->render('pages/view.html.twig', ['post' => $post]);
     }
 
     /**
      * @Route("/edit/{id}", name="edit_post_route")
      */
-    public function editPostAction($id)
+    public function editPostAction(Request $request, $id)
     {
-        return $this->render('pages/edit.html.twig');
+        $post = $this->getDoctrine()->getRepository('AppBundle:Post')->find($id);
+        $post->setTitle($post->getTitle());
+        $post->setDescription($post->getDescription());
+        $post->setCategory($post->getCategory());
+
+        $form = $this->createFormBuilder($post)
+        ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control')))
+        ->add('category', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array('label' => 'Update Post', 'attr' => array('class' => 'btn btn-success', 'style' => 'margin-top:20px')))
+        ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $title = $form['title']->getData();
+            $description = $form['description']->getData();
+            $category = $form['category']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $post = $em->getRepository('AppBundle:Post')->find($id);
+
+            $post->setTitle($title);
+            $post->setDescription($description);
+            $post->setCategory($category);
+
+            $em->flush();
+            $this->addFlash('message', 'Post Updated Successfully');
+            return $this->redirectToRoute('show_post_route');
+
+        }
+
+        return $this->render('pages/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
@@ -45,7 +112,12 @@ class PostController extends Controller
      */
     public function deletePostsAction($id)
     {
-        return $this->render('pages/delete.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('AppBundle:Post')->find($id);
+        $em->remove($post);
+        $em->flush();
+        $this->addFlash('message', 'Post Deleted Successfully');
+        return $this->redirectToRoute('show_post_route');
     }
 
 
